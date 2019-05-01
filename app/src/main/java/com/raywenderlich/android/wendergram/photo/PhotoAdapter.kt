b17542415e8ff038dbcd9db5ab3e92703e6dfc0b
taingmeng <taing.meng@2359media.com>
2019-05-01 09:57:16 +0800

    Make listener in adapter private

--- app/src/main/java/com/raywenderlich/android/wendergram/photo/PhotoAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/wendergram/photo/PhotoAdapter.kt
@@ -37,7 +37,7 @@ import androidx.recyclerview.widget.RecyclerView
 class PhotoAdapter(private val list: List<String?>)
   : RecyclerView.Adapter<PhotoViewHolder>() {
 
-  var recyclerViewItemListener: ((String?) -> Unit)? = null
+  private var recyclerViewItemListener: ((String?) -> Unit)? = null
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
     val inflater = LayoutInflater.from(parent.context)
